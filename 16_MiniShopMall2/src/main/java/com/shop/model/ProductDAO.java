4208c768aa6103fa9b163a8d4eed8f6672d6af5b
HSH96\clxkd <clxkdi123@gmail.com>
2024-04-12 18:24:23 +0900

    16 미니쇼핑몰

---
+++ 16_MiniShopMall2/src/main/java/com/shop/model/ProductDAO.java
@@ -0,0 +1,433 @@
+package com.shop.model;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
+
+
+public class ProductDAO {
+
+	// DB와 연동하는 객체.
+	Connection con = null;
+	
+	// DB에 SQL문을 전송하는 객체.
+	PreparedStatement pstmt = null;
+	
+	// SQL문을 실행한 후에 결과 값을 가지고 있는 객체.
+	ResultSet rs, rs1 = null;
+	
+	// 쿼리문을 저장할 변수
+	String sql = null;
+	
+	
+	// ProductDAO 클래스를 싱글턴 방식으로 만들어 보자.
+	// 1단계 : 싱글턴 방식을 객체를 만들기 위해서는 우선적으로
+	//        기본생성자의 접근제어자를 public 이 아니라
+	//        private 으로 바꾸어 주어야 한다.
+	//        즉, 외부에서는 직접적으로 기본생성자를 호출하지
+	//        못하게 하는 방식이다.
+	
+	// 2단계 : ProductDAO 클래스를 정적(static) 멤버로 
+	//        선언을 해 주어야 한다.
+	private static ProductDAO instance;
+	
+	private ProductDAO() {  }  // 기본 생성자
+	
+	// 3단계 : 기본생성자 대신에 싱글턴 객체를 return 해 주는
+	//        getInstance() 메서드를 만들어서 해당 
+	//        getInstance() 메서드를 외부에서 접근할 수 있도록
+	//        해 주면 됨.
+	public static ProductDAO getInstance() {
+		
+		if(instance == null) {
+			instance = new ProductDAO();
+		}
+		
+		return instance;
+	}  // getInstance() 메서드 end
+	
+	
+	// DB를 연동하는 작업을 진행하는 메서드.
+	public void openConn() {
+		
+		try {
+			// 1단계 : JNDI 서버 객체 생성
+			// 자바의 네이밍 서비스(JNDI)애서 이름과 실제 객체를
+			// 연결해 주는 개념이 Context 객체이며, InitialContext
+			// 객체는 네이밍 서비스를 이용하기 위한 시작점이 됨.
+			Context initCtx = new InitialContext();
+			
+			// 2단계 : Context 객체를 얻어와야 함.
+			// "java:comp/env" 라는 이름의 인수로 Context 객체를
+			// 얻어옴. "java:comp/env"는 현재 웹 애플리케이션에서
+			// 네이밍 서비스를 이용 시 루트 디렉토리라고 생각하면 됨.
+			// 즉, 현재 웹 애플리케이션이 사용할 수 있는 모든 자원은
+			// "java:comp/env" 아래에 위치를 하게 됨.
+			Context ctx = 
+				(Context)initCtx.lookup("java:comp/env");
+			
+			// 3단계 : lookup() 메서드를 이용하여 매칭되는 커넥션을
+			//        찾게 됨.
+			// "java:comp/env" 아래에 위치한 "jdbc/myoracle"
+			// 자원을 얻어옴, 이자원이 바로 데이터소스(커넥션풀)임.
+			// 여기서 "jdbc/myoracle" 은 context.xml 파일에
+			// 추가했던 <Resource> 태그 안에 있던 name 속성의 값임.
+			DataSource ds = 
+					(DataSource)ctx.lookup("jdbc/myoracle");
+			
+			// 4단계 : DataSource 객체를 이용하여 커넥션을
+			//        하나 가져온다.
+			con = ds.getConnection();
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}  // openConn() 메서드 end
+		
+	
+	// DB에 연결되어 있던 자원 종료하는 메서드.
+	public void closeConn(ResultSet rs,
+			PreparedStatement pstmt, Connection con) {
+		
+			try {
+				if(rs != null) rs.close();
+				if(pstmt != null) pstmt.close();
+				if(con != null) con.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		
+	}  // closeConn() 메서드 end
+	
+	
+	// DB에 연결되어 있던 자원 종료하는 메서드.
+	public void closeConn(
+			PreparedStatement pstmt, Connection con) {
+		
+			try {
+				if(pstmt != null) pstmt.close();
+				if(con != null) con.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		
+	}  // closeConn() 메서드 end
+	
+	
+	// shop_products 테이블에 상품을 등록(추가)하는 메서드.
+	public int insertProduct(ProductDTO dto) {
+		
+		int result = 0, count = 0;
+		
+		
+		try {
+			openConn();
+			
+			sql = "select max(pnum) "
+					+ " from shop_products";
+			
+			pstmt = con.prepareStatement(sql);
+			
+			rs = pstmt.executeQuery();
+			
+			if(rs.next()) {
+				
+				count = rs.getInt(1) + 1;
+			}
+			
+			sql = "insert into shop_products "
+					+ " values(?, ?, ?, ?, ?, ?, "
+					+ " ?, ?, ?, ?, sysdate)";
+			
+			pstmt = con.prepareStatement(sql);
+			
+			pstmt.setInt(1, count);
+			pstmt.setString(2, dto.getPname());
+			pstmt.setString(3, dto.getPcategory_fk());
+			pstmt.setString(4, dto.getPcompany());
+			pstmt.setString(5, dto.getPimage());
+			pstmt.setInt(6, dto.getPqty());
+			pstmt.setInt(7, dto.getPrice());
+			pstmt.setString(8, dto.getPspec());
+			pstmt.setString(9, dto.getPcontents());
+			pstmt.setInt(10, dto.getPoint());
+			
+			result = pstmt.executeUpdate();
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			closeConn(rs, pstmt, con);
+		}
+		
+		return result;
+	}  // insertProduct() 메서드 end
+	
+	
+	
+	// 전체 상품 목록을 조회하는 메서드.
+	public List<ProductDTO> getProductList() {
+		
+		List<ProductDTO> list = 
+						new ArrayList<ProductDTO>();
+		
+		
+		try {
+			openConn();
+			
+			sql = "select * from shop_products "
+					+ " order by pnum desc";
+			
+			pstmt = con.prepareStatement(sql);
+			
+			rs = pstmt.executeQuery();
+			
+			while(rs.next()) {
+				
+				ProductDTO dto = new ProductDTO();
+				
+				dto.setPnum(rs.getInt("pnum"));
+				dto.setPname(rs.getString("pname"));
+				dto.setPcategory_fk(rs.getString("pcategory_fk"));
+				dto.setPcompany(rs.getString("pcompany"));
+				dto.setPimage(rs.getString("pimage"));
+				dto.setPqty(rs.getInt("pqty"));
+				dto.setPrice(rs.getInt("price"));
+				dto.setPspec(rs.getString("pspec"));
+				dto.setPcontents(rs.getString("pcontents"));
+				dto.setPoint(rs.getInt("point"));
+				dto.setPinputdate(rs.getString("pinputdate"));
+				
+				
+				list.add(dto);
+				
+			}
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			closeConn(rs, pstmt, con);
+		}
+		
+		return list;
+	}  // getProductList() 메서드 end
+	
+	
+	
+	// 제품번호에 해당하는 제품의 상세 정보를 조회하는 메서드.
+	public ProductDTO getProductContent(int pnum) {
+		
+		ProductDTO dto = null;
+		
+		
+		try {
+			openConn();
+			
+			sql = "select * from shop_products "
+					+ " where pnum = ?";
+			
+			pstmt = con.prepareStatement(sql);
+			
+			pstmt.setInt(1, pnum);
+			
+			rs = pstmt.executeQuery();
+			
+			if(rs.next()) {
+				
+				dto = new ProductDTO();
+				
+				dto.setPnum(rs.getInt("pnum"));
+				dto.setPname(rs.getString("pname"));
+				dto.setPcategory_fk(rs.getString("pcategory_fk"));
+				dto.setPcompany(rs.getString("pcompany"));
+				dto.setPimage(rs.getString("pimage"));
+				dto.setPqty(rs.getInt("pqty"));
+				dto.setPrice(rs.getInt("price"));
+				dto.setPspec(rs.getString("pspec"));
+				dto.setPcontents(rs.getString("pcontents"));
+				dto.setPoint(rs.getInt("point"));
+				dto.setPinputdate(rs.getString("pinputdate"));
+			
+			}
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			closeConn(rs, pstmt, con);
+		}
+		
+		return dto;
+	}  // getProductContent() 메서드 end
+	
+	
+	// 제품번호에 해당하는 제품의 정보를 수정하는 메서드.
+	public int updateProduct(ProductDTO dto) {
+		
+		int result = 0;
+		
+		
+		try {
+			openConn();
+			
+			sql = "update shop_products set "
+					+ " pimage = ?, pqty = ?, "
+					+ " price = ?, pspec = ?, "
+					+ " pcontents = ?, point = ? "
+					+ " where pnum = ?";
+			
+			pstmt = con.prepareStatement(sql);
+			
+			pstmt.setString(1, dto.getPimage());
+			pstmt.setInt(2, dto.getPqty());
+			pstmt.setInt(3, dto.getPrice());
+			pstmt.setString(4, dto.getPspec());
+			pstmt.setString(5, dto.getPcontents());
+			pstmt.setInt(6, dto.getPoint());
+			pstmt.setInt(7, dto.getPnum());
+			
+			result = pstmt.executeUpdate();
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			closeConn(pstmt, con);
+		}
+		
+		return result;
+	}  // updateProduct() 메서드 end
+	
+	
+	
+	// 제품번호에 해당하는 제품을 테이블에서 삭제하는 메서드.
+	public int deleteProduct(int pnum) {
+		
+		int result = 0;
+		
+		
+		try {
+			openConn();
+			
+			sql = "delete from shop_products "
+					+ " where pnum = ?";
+			
+			pstmt = con.prepareStatement(sql);
+			
+			pstmt.setInt(1, pnum);
+			
+			result = pstmt.executeUpdate();
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			closeConn(pstmt, con);
+		}
+		
+		return result;
+	}  // deleteProduct() 메서드 end
+
+	// 제품 삭제 시 제품 번호 재작업 하는 메서드.
+	public void updateSequence(int product_no) {
+		
+		
+		try {
+			openConn();
+			
+			sql = "update shop_products "
+					+ " set pnum = pnum - 1 "
+					+ " where pnum > ?";
+			
+			pstmt = con.prepareStatement(sql);
+			
+			pstmt.setInt(1, product_no);
+			
+			pstmt.executeUpdate();
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			closeConn(pstmt, con);
+		}
+		
+	}  // updateSequence() 메서드 end
+	
+	
+	
+	// 카테고리 코드에 해당하는 제품의 전체 리스트를 조회하는 메서드.
+	public List<ProductDTO> getProductList(String code) {
+		
+		List<ProductDTO> list = new ArrayList<ProductDTO>();
+		
+		try {
+			openConn();
+			
+			sql = "select * from shop_products "
+					+ " where pcategory_fk = ?";
+			
+			pstmt = con.prepareStatement(sql);
+			
+			pstmt.setString(1, code);
+			
+			rs = pstmt.executeQuery();
+			
+			while(rs.next()) {
+				
+				ProductDTO dto = new ProductDTO();
+				
+				dto.setPnum(rs.getInt("pnum"));
+				dto.setPname(rs.getString("pname"));
+				dto.setPcategory_fk(rs.getString("pcategory_fk"));
+				dto.setPcompany(rs.getString("pcompany"));
+				dto.setPimage(rs.getString("pimage"));
+				dto.setPqty(rs.getInt("pqty"));
+				dto.setPrice(rs.getInt("price"));
+				dto.setPspec(rs.getString("pspec"));
+				dto.setPcontents(rs.getString("pcontents"));
+				dto.setPoint(rs.getInt("point"));
+				dto.setPinputdate(rs.getString("pinputdate"));
+				
+				
+				list.add(dto);
+				
+			}
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			closeConn(rs, pstmt, con);
+		}
+		
+		return list;
+	}  // getProductList() 메서드 end
+	
+	
+	
+	
+	
+	
+	
+	
+}
+
+
+
+
+
+
+
+
